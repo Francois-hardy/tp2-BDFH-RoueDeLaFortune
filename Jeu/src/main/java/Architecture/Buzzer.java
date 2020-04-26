1de39db394e048e0b76c62258fac99e81dcfa139
Julian <ferrarinijulian@gmail.com>
2020-04-26 22:29:11 +0200

    Suppression de l'interface

--- Jeu/src/main/java/Architecture/Buzzer.java
+++ Jeu/src/main/java/Architecture/Buzzer.java
@@ -7,14 +7,18 @@ import javax.swing.*;
 public class Buzzer extends JFrame implements ActionListener {
 
     public JButton bouton = new JButton("Buzzer");
+    public JButton bouton1 = new JButton(new GetAction(this, "Je valide"));
     public static Integer boutonON = 0;
+
+    public JTextField textField;
+    public JLabel label;
 
     public static int getBoutonON(){
         return boutonON;
     }
 
     public Buzzer(){
-        this.setTitle("Roue de ta daronne");
+        this.setTitle("Roue de la fortune");
         this.setSize(400, 100);
         this.setLocationRelativeTo(null);
 
@@ -26,8 +30,6 @@ public class Buzzer extends JFrame implements ActionListener {
         //On prévient notre JFrame que notre JPanel sera son content pane
 
         JLabel label = new JLabel(" Il faut buzzer si vous avez le mot");
-
-
 
 
         pan.add(label);
@@ -44,11 +46,26 @@ public class Buzzer extends JFrame implements ActionListener {
         this.setTitle("Votre réponse");
         this.setSize(400, 100);
         this.setLocationRelativeTo(null);
+
         JPanel pan2 = new JPanel();
-        JTextField textField = new JTextField();
+
+        textField = new JTextField();
         textField.setColumns(10); //On lui donne un nombre de colonnes à afficher
+        label = new JLabel("Votre réponse");
+
         pan2.add(textField);
+        pan2.add(bouton1);
+        pan2.add(label);
         this.setContentPane(pan2);
         this.setVisible(true);
+
+    }
+
+    public JTextField getTextField(){
+        return textField;
+    }
+
+    public JLabel getLabel(){
+        return label;
     }
 }
